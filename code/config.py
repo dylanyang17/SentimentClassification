c2bd11ce6ac3fb9b102d789d1b55a3e672e4d4a1
yangyr17 <315629555@qq.com>
2020-05-31 11:15:57 +0800

    save

--- code/config.py
+++ code/config.py
@@ -7,9 +7,13 @@ class Config:
     vec_path = os.path.join(data_root, 'sgns.sogounews.bigram-char')
     reduced_vec_path = os.path.join(data_root, 'reduced.sgns.sogounews.bigram-char')
     train_data_path = os.path.join(data_root, 'sinanews.train')
+    valid_data_path = os.path.join(data_root, 'sinanews.valid')
     test_data_path = os.path.join(data_root, 'sinanews.test')
     label_len = 8
     vec_len = 300
     seq_len = 300
     unk_vec = torch.rand([vec_len])*2 - 1
-    train_batch_size = 32
+    train_batch_size = 16
+    lr = 0.001
+    momentum = 0.9
+    epoch_num = 100
--- code/train.py
+++ code/train.py
@@ -1,5 +1,8 @@
-import torch
 import os
+import time
+
+import torch
+from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as transforms
 from config import Config
@@ -14,10 +17,10 @@ def load_word2vec(path):
         print(word_num, vec_len)
         word2vec = {}
         for i in range(1, word_num + 1):
-            if i % 500 == 0:
+            if i % 5000 == 0:
                 print('%d / %d' % (i, word_num))
             items = lines[i].strip().split(' ')
-            vec = torch.tensor(map(float, items[1:vec_len+1]))
+            vec = torch.tensor(list(map(float, items[1:vec_len+1])))
             word2vec[items[0]] = vec
     print('Loaded word2vec successfully.')
     return word2vec
@@ -50,19 +53,119 @@ class MyDataset(Dataset):
                     data_item[j] = vec
                 self.data.append(data_item)
 
-                label_item = torch.zeros([label_len])
+                label_item = torch.zeros([label_len], dtype=torch.float)
                 sen = tmp[1].split(':')
-                for j in range(1, label_len+1):
-                    label_item[j-1] = float(re.match('([0-9]*).*', sen[j]).group(1))
+                for j in range(2, label_len+2):
+                    label_item[j-2] = float(re.match('([0-9]*).*', sen[j]).group(1))
                 label_item /= label_item.sum()
+                self.labels.append(label_item)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        return self.data[index], self.labels[index]
+        return self.data[index], self.text_lengths[index], self.labels[index]
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv1d(Config.seq_len, 200, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm1d(200),
+            nn.Dropout(p=.2),
+
+            nn.Conv1d(200, 100, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Dropout(p=.2),
+
+            nn.Conv1d(100, 16, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm1d(16),
+            nn.Dropout(p=.2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16 * Config.vec_len, 100),
+            nn.ReLU(),
+            nn.Linear(100, 8),
+            nn.Tanh()
+        )
+
+    def forward(self, input_data):
+        output = self.cnn(input_data)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+
+
+def calc_acc(net, device, dataloaders):
+    ret = []
+    with torch.no_grad():
+        for dataloader in dataloaders:
+            correct = torch.zeros([1]).to(device)
+            total = torch.zeros([1]).to(device)
+            for i, data in enumerate(dataloader):
+                inputs, _, labels = data
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = net(inputs)
+                prediction = torch.argmax(outputs, 1)
+                correct += (prediction == torch.argmax(labels, 1)).sum().float()
+                total += len(labels)
+            ret.append(float(correct/total))
+    return ret
+
+
+def train(net, device, optimizer, criterion, train_dataloader, test_dataloader):
+    if hasattr(torch.cuda, 'empty_cache'):
+        torch.cuda.empty_cache()
+
+    now = time.clock()
+    for epoch in range(Config.epoch_num):
+        running_loss = 0.0
+        cnt = 0
+        for i, data in enumerate(train_dataloader):
+            inputs, _, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            cnt += 1
+
+        delta = time.clock() - now
+        now = time.clock()
+        train_acc, test_acc = calc_acc(net, device, [train_dataloader, test_dataloader])
+        print('[%d] loss: %.3f  cost: %.3f  train_acc: %.3f  test_acc: %.3f' %
+              (epoch + 1, running_loss / cnt, delta, train_acc, test_acc))
+
+
+def train_cnn():
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    net = CNN().to(device)
+    optimizer = optim.SGD(net.parameters(), lr=Config.lr, momentum=Config.momentum)
+    criterion = nn.MSELoss()
+    train(net, device, optimizer, criterion, train_dataloader, test_dataloader)
+
+
+def train_rnn():
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    net = RNN().to(device)
+    optimizer = optim.SGD(net.parameters(), lr=Config.lr, momentum=Config.momentum)
+    criterion = nn.MSELoss()
+    train(net, device, optimizer, criterion, train_dataloader, test_dataloader)
 
 
 if __name__ == '__main__':
     word2vec = load_word2vec(Config.reduced_vec_path)
     train_data = MyDataset(Config.train_data_path, Config.seq_len, Config.vec_len, Config.label_len, word2vec)
+    test_data = MyDataset(Config.test_data_path, Config.seq_len, Config.vec_len, Config.label_len, word2vec)
+    train_dataloader = DataLoader(dataset=train_data, shuffle=True, num_workers=0, batch_size=Config.train_batch_size)
+    test_dataloader = DataLoader(dataset=test_data, shuffle=True, num_workers=0, batch_size=Config.train_batch_size)
+
+    train_rnn()
